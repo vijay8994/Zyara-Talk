8d96e39b820eb2d64e6ace95c32a74e28a9bbcdc
vijay8994 <vijay.clavis@gmail.com>
2023-03-03 16:30:20 +0530

    new changes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -26,3 +26,41 @@ $(document).ready(function() {$('#tabs').on('click', 'div', function() {
         $(this).addClass('act');
     });
 });
+
+// One
+$('#colorpicker').on('input', function() {
+    $('#hexcolor').val(this.value);
+});
+$('#hexcolor').on('input', function() {
+  $('#colorpicker').val(this.value);
+});
+
+
+// TWO
+$('#colorpickerTwo').on('input', function() {
+    $('#hexcolorTwo').val(this.value);
+});
+$('#hexcolorTwo').on('input', function() {
+  $('#colorpickerTwo').val(this.value);
+});
+
+
+// THREE
+$('#colorpickerThree').on('input', function() {
+    $('#hexcolorThree').val(this.value);
+});
+$('#hexcolorThree').on('input', function() {
+  $('#colorpickerThree').val(this.value);
+});
+
+
+// Four
+$('#colorpickerFour').on('input', function() {
+    $('#hexcolorFour').val(this.value);
+});
+$('#hexcolorFour').on('input', function() {
+  $('#colorpickerFour').val(this.value);
+});
+
+
+
